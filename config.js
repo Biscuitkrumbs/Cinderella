012742688b51e98769e06fa5b7686012ed12bc96
Biscuitkrumbs <136212712+Biscuitkrumbs@users.noreply.github.com>
2026-06-30 16:48:53 +1000

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
 // Cinderella configuration
-
-const SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbxkxlk-pGuupVQhv39kHo5QINuOi4bAy1nJfhDDbJ2XWtH4rimOR8iGFtvtaPU9Wor-6g/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxkxlk-pGuupVQhv39kHo5QINuOi4bAy1nJfhDDbJ2XWtH4rimOR8iGFtvtaPU9Wor-6g/exec";
